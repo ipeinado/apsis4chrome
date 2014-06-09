01b52fc59a563a35a68b10ae53656f6a6f624074
Ignacio Peinado <i.peinado.martinez@gmail.com>
2014-06-09 17:09:45 +0200

    dummy simplifier

--- js/simplifier.js
+++ js/simplifier.js
@@ -1,15 +1,36 @@
 (function() {
 
 	var divContainer = $("<div class='simple-container'></ul>");
-	var firstLevelUL = $("<ul class='first-level'></ul>")
+	var counter = 0;
 	
-	$("body *").not("div, script, ul, li, input").each(function() {
-		if ($(this).text().length > 0) {
-			$(divContainer).append(this);	
+	$(":header, p, blockquote, article, ul, img").each(function() {
+		if ($(this).prop("tagName") == "UL") {
+			counter++;
+			$(this).addClass("collapsed-list").attr("id", "list_" + counter.toString());
+
+			var button = $("<a>[List]</a>").addClass("expand-button").attr("id", "list_" + counter.toString());
+			$(divContainer).append(button);
+
 		}
-		
+		$(divContainer).append(this);
 	});
 
 	$("body").html(divContainer);
 
 })();
+
+//	$(".collapsed-list").children().css("display", "none");
+$(".collapsed-list").attr("title", "click on the list to expand it");
+
+$(".expand-button").click(function(e) {
+	e.preventDefault();
+	var listId = $(this).attr("id");
+	var ul = $("ul").filter(function() {
+		return ($(this).attr("id") == listId); 
+	});
+	$(ul[0]).toggle();
+});
+
+$(".collapsed-list").css("display", "none");
+
+
